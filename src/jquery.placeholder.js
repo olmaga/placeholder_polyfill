e97db62cf04a0b32bc6dc264262c7939e36d48da
Oliver Ganz <oliver.ganz@gmail.com>
2012-12-17 13:41:32 +0100

    added new calculation of the position
    can still be off by 1px

--- src/jquery.placeholder.js
+++ src/jquery.placeholder.js
@@ -18,13 +18,13 @@
     }
     function positionPlaceholder (placeholder, label, input) {
         var ta = input.is('textarea');
-
+      
         // padding + margin + border
         var padleft = parseInt(input.css("padding-left").replace("px"), 10);
         var margleft = parseInt(input.css("margin-left").replace("px"), 10);
         var bordleft = parseInt(input.css("border-left-width").replace("px"), 10);
         var left = (padleft ? padleft : 0) + (margleft ? margleft : 0) + (bordleft ? bordleft : 0);
-
+      
         // just seems to be right to do one pixel less
         var padtop = parseInt(input.css("padding-top").replace("px"), 10);
         var margtop = parseInt(input.css("margin-top").replace("px"), 10);
@@ -32,8 +32,8 @@
         var top = (padtop ? padtop : 0) + (margtop ? margtop : 0) + (bordtop ? bordtop : 0) + 1;
 
         // inputs have just the height of the input as line-height to be vertically aligned
-        var lineheight = input.outerHeight();
-        if (ta) {
+        var lineheight = input.innerHeight();
+        if (ta) {  
             // if a textarea, the lineheight is set to either the line-height of the textarea (if set)
             var lh = parseInt(input.css("line-height").replace("px"), 10);
             if (isNaN(lh)) {
@@ -41,10 +41,13 @@
                 lineheight = parseInt(input.css("font-size").replace("px"), 10) * 1.3;
             } else {
                 lineheight = lh;
-            }
-            // and we add the padding/margin/border
-            lineheight += top * 2;
+            } 
+            // and we add the padding/margin/border (twice for textareas)
+            lineheight += top;
         }
+        // and we add the padding/margin/border
+        lineheight += top;
+      
         placeholder.css({
             "font-size": input.css("font-size"),
             "line-height": lineheight + "px",
@@ -64,7 +67,7 @@
                 startEmptiedCheckChange(input, label, options);
             }
         }());
-    }
+    } 
     function startEmptiedCheckChange (input, label, options) {
         (function checkloop () {
             animId = requestAnimationFrame(checkloop);
@@ -114,7 +117,7 @@
             // already added a polyfill?
             var polyfilled = $(label).find('.placeholder');
             if (polyfilled.size() > 0) {
-                positionPlaceholder(polyfilled, label, input);
+                positionPlaceholder(polyfilled, label,  input);
                 polyfilled.text(txt);
                 return input;
             }
@@ -134,14 +137,10 @@
 
             // pass the focus onclick ie7 needs a timeout here
             label.click(function () {
-                setTimeout(function () {
-                    input.focus();
-                }, 1);
+                setTimeout(function() { input.focus(); }, 1);
             });
             placeholder.click(function () {
-                setTimeout(function () {
-                    input.focus();
-                }, 1);
+                setTimeout(function() { input.focus(); }, 1);
             });
 
             positionPlaceholder(placeholder, label, input);
@@ -163,12 +162,12 @@
             if (window.requestAnimationFrame) {
                 startFilledCheckChange(input, label, o.options);
             }
-
+            
             showPlaceholderIfEmpty(input, label, o.options);
 
             // reformat on window resize and optional reformat on font resize - requires: http://www.tomdeater.com/jquery/onfontresize/
             $(document).bind("fontresize resize", function () {
-                positionPlaceholder(placeholder, label, input);
+                positionPlaceholder(placeholder, label,  input);
             });
             return this;
         });
